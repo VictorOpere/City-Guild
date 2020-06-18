b209da5973f637c74a16645363445186299f2d64
Victor Opere <victoropere@gmail.com>
2020-06-18 07:55:23 +0300

    just added forgot password and make selection password

---
+++ app/src/main/java/com/example/cityguild/Common/LoginSignup/MakeSelection.java
@@ -0,0 +1,15 @@
+package com.example.cityguild.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.cityguild.R;
+
+public class MakeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_selection);
+    }
+}
