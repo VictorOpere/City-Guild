e465fdab76aebb66e184278846a6239e2603b164
Victor Opere <victoropere@gmail.com>
2020-06-19 07:37:12 +0300

    just added firebase

--- app/src/main/java/com/example/cityguild/Common/LoginSignup/SignUp3rdClass.java
+++ app/src/main/java/com/example/cityguild/Common/LoginSignup/SignUp3rdClass.java
@@ -7,18 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.cityguild.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
 
 
 public class SignUp3rdClass extends AppCompatActivity {
+
+//    variables
+    ScrollView scrollView;
+    TextInputLayout phoneNumber;
+    CountryCodePicker countryCodePicker;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up3rd_class);
+
+//        hooks
+
+        scrollView = findViewById(R.id.signup_3rd_scrollview);
+        phoneNumber = findViewById(R.id.signup_phone_number);
+        countryCodePicker = findViewById(R.id.country_code_picker);
+
 
         Intent intent = new Intent();
         String getFullName = intent.getStringExtra("fullname");
@@ -30,6 +46,20 @@ public class SignUp3rdClass extends AppCompatActivity {
 
     }
 
+//    call Verify OTP screen
+
+    public void callVerifyOTPScreen(){
+
+        if (!validatePhoneNumber()){
+
+            return;
+        }
+
+        startActivity(new Intent(getApplicationContext(),VerifyOTP.class));
+        finish();
+
+
+    }
 
     //    login function
 
@@ -38,6 +68,32 @@ public class SignUp3rdClass extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),Login.class);
         startActivity(intent);
         finish();
+
+    }
+
+//    validate phone number
+
+    private boolean validatePhoneNumber(){
+
+        String value = phoneNumber.getEditText().getText().toString().trim();
+        String checkEmail = "[0-9]";
+
+        if (value.isEmpty()){
+
+            phoneNumber.setError("Field Cannot Be Empty");
+            return false;
+        }else if(!value.matches(checkEmail)){
+
+            phoneNumber.setError("Invalid Email!");
+            return false;
+
+        }else {
+
+            phoneNumber.setError(null);
+            phoneNumber.setErrorEnabled(false);
+            return true;
+
+        }
 
     }
 
